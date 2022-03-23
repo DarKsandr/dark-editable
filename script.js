4db28b7ca7f2d11d5642ff313ef9f9dab789f3bc
DarKsandr <gdarksandr@gmail.com>
2022-03-23 11:29:33 +0400

    reorganization

--- script.js
+++
@@ -1,7 +0,0 @@
-// import { DarkEditable } from "./dark-editable/dark-editable.js";
-
-const text = document.getElementById("text");
-
-const popover = new DarkEditable(text, {
-    type: "text",
-});
