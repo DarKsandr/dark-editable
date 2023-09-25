6fe770d6aa3ff74de42ec382fdc3480fb239192e
volkov <aleksandr.volkov@ma.ru>
2023-09-25 14:02:15 +0300

    Add inline mode

--- dist/dark-editable.js
+++ dist/dark-editable.js
@@ -1,20 +1,122 @@
-class d {
+var m = Object.defineProperty;
+var p = (o, e, t) => e in o ? m(o, e, { enumerable: !0, configurable: !0, writable: !0, value: t }) : o[e] = t;
+var u = (o, e, t) => (p(o, typeof e != "symbol" ? e + "" : e, t), t);
+class h {
+  constructor(e) {
+    if (this.constructor === h)
+      throw new Error("It's abstract class");
+    this.context = e;
+  }
+  event_show() {
+    this.context.hideError(), this.context._element.element.value = this.context.value, this.context.element.dispatchEvent(new CustomEvent("show"));
+  }
+  event_shown() {
+    this.context.element.dispatchEvent(new CustomEvent("shown"));
+  }
+  event_hide() {
+    this.context.element.dispatchEvent(new CustomEvent("hide"));
+  }
+  event_hidden() {
+    this.context.element.dispatchEvent(new CustomEvent("hidden"));
+  }
+  init() {
+    throw new Error("Method `init` not define!");
+  }
+  enable() {
+    throw new Error("Method `enable` not define!");
+  }
+  disable() {
+    throw new Error("Method `disable` not define!");
+  }
+  hide() {
+    throw new Error("Method `hide` not define!");
+  }
+}
+class v extends h {
+  init() {
+    this.popover = new bootstrap.Popover(this.context.element, {
+      container: "body",
+      content: this.context.route_type(),
+      html: !0,
+      customClass: "dark-editable",
+      title: this.context.title
+    }), this.context.element.addEventListener("show.bs.popover", () => {
+      this.event_show();
+    }), this.context.element.addEventListener("shown.bs.popover", () => {
+      this.event_shown();
+    }), this.context.element.addEventListener("hide.bs.popover", () => {
+      this.event_hide();
+    }), this.context.element.addEventListener("hidden.bs.popover", () => {
+      this.event_hidden();
+    }), document.addEventListener("click", (e) => {
+      const t = e.target;
+      if (t === this.popover.tip || t === this.context.element)
+        return;
+      let s = t;
+      for (; s = s.parentNode; )
+        if (s === this.popover.tip)
+          return;
+      this.hide();
+    });
+  }
+  enable() {
+    this.popover.enable();
+  }
+  disable() {
+    this.popover.disable();
+  }
+  hide() {
+    this.popover.hide();
+  }
+}
+class f extends h {
+  init() {
+    const e = () => {
+      if (!this.context.disabled) {
+        const t = this.context.route_type();
+        this.event_show(), this.context.element.removeEventListener("click", e), this.context.element.innerHTML = "", this.context.element.append(t), this.event_shown();
+      }
+    };
+    this.context.element.addEventListener("click", e);
+  }
+  enable() {
+  }
+  disable() {
+  }
+  hide() {
+    this.event_hide(), this.context.element.innerHTML = this.context.value, setTimeout(() => {
+      this.init(), this.event_hidden();
+    }, 100);
+  }
+}
+class E {
   constructor(e, t = {}) {
-    this._element = { element: null, form: null, load: null, buttons: { success: null, cancel: null } }, this.element = e, this.options = t, this.init_options(), this.init_popover(), this.init_text(), this.init_hide_onclick(), this.init_style(), this.disabled && this.disable(), this.element.dispatchEvent(new CustomEvent("init"));
+    u(this, "modeElement", null);
+    switch (this._element = { element: null, form: null, load: null, buttons: { success: null, cancel: null } }, this.element = e, this.options = t, this.init_options(), this.mode) {
+      default:
+        throw new Error(`Mode ${this.mode} not found!`);
+      case "popup":
+        this.modeElement = new v(this);
+        break;
+      case "inline":
+        this.modeElement = new f(this);
+        break;
+    }
+    this.modeElement.init(), this.init_text(), this.init_style(), this.disabled && this.disable(), this.element.dispatchEvent(new CustomEvent("init"));
   }
   /* INIT METHODS */
   init_options() {
-    var s, i, a, r;
-    const e = (n, o) => {
-      var l, h;
-      return this[n] = ((l = this.element.dataset) == null ? void 0 : l[n]) ?? ((h = this.options) == null ? void 0 : h[n]) ?? o;
-    }, t = (n, o) => (e(n, o), typeof this[n] != "boolean" && (this[n] == "true" ? this[n] = !0 : this[n] == "false" ? this[n] = !1 : this[n] = o), this[n]);
-    switch (e("value", this.element.innerHTML), e("name", this.element.id), e("pk", null), e("title", ""), e("type", "text"), e("emptytext", "Empty"), e("url", null), e("ajaxOptions", {}), this.ajaxOptions = Object.assign({
+    var s, n, a, r;
+    const e = (i, l) => {
+      var c, d;
+      return this[i] = ((c = this.element.dataset) == null ? void 0 : c[i]) ?? ((d = this.options) == null ? void 0 : d[i]) ?? l;
+    }, t = (i, l) => (e(i, l), typeof this[i] != "boolean" && (this[i] === "true" ? this[i] = !0 : this[i] === "false" ? this[i] = !1 : this[i] = l), this[i]);
+    switch (e("value", this.element.innerHTML), e("name", this.element.id), e("pk", null), e("title", ""), e("type", "text"), e("emptytext", "Empty"), e("mode", "popup"), e("url", null), e("ajaxOptions", {}), this.ajaxOptions = Object.assign({
       method: "POST",
       dataType: "text"
-    }, this.ajaxOptions), t("send", !0), t("disabled", !1), t("required", !1), (s = this.options) != null && s.success && typeof ((i = this.options) == null ? void 0 : i.success) == "function" && (this.success = this.options.success), (a = this.options) != null && a.error && typeof ((r = this.options) == null ? void 0 : r.error) == "function" && (this.error = this.options.error), this.type) {
+    }, this.ajaxOptions), t("send", !0), t("disabled", !1), t("required", !1), (s = this.options) != null && s.success && typeof ((n = this.options) == null ? void 0 : n.success) == "function" && (this.success = this.options.success), (a = this.options) != null && a.error && typeof ((r = this.options) == null ? void 0 : r.error) == "function" && (this.error = this.options.error), this.type) {
       case "select":
-        e("source", []), typeof this.source == "string" && this.source != "" && (this.source = JSON.parse(this.source));
+        e("source", []), typeof this.source == "string" && this.source !== "" && (this.source = JSON.parse(this.source));
         break;
       case "date":
         e("format", "YYYY-MM-DD"), e("viewformat", "YYYY-MM-DD");
@@ -30,51 +132,22 @@ class d {
     let t = !0;
     switch (this.type) {
       default:
-        this.value == "" ? this.element.innerHTML = this.emptytext : (this.element.innerHTML = this.value, t = !1);
+        this.value === "" ? this.element.innerHTML = this.emptytext : (this.element.innerHTML = this.value, t = !1);
         break;
       case "select":
-        this.element.innerHTML = this.emptytext, this.value != "" && this.source.length > 0 && this.source.forEach((s) => {
+        this.element.innerHTML = this.emptytext, this.value !== "" && this.source.length > 0 && this.source.forEach((s) => {
           s.value == this.value && (this.element.innerHTML = s.text, t = !1);
         });
         break;
       case "date":
       case "datetime":
-        this.value == "" ? this.element.innerHTML = this.emptytext : (this.element.innerHTML = moment(this.value).format(this.viewformat), t = !1);
+        this.value === "" ? this.element.innerHTML = this.emptytext : (this.element.innerHTML = moment(this.value).format(this.viewformat), t = !1);
         break;
     }
     t && this.element.classList.add(e);
   }
   init_style() {
     this.element.classList.add("dark-editable-element");
-  }
-  init_hide_onclick() {
-    document.addEventListener("click", (e) => {
-      const t = e.target;
-      if (t === this.popover.tip || t == this.element)
-        return;
-      let s = t;
-      for (; s = s.parentNode; )
-        if (s === this.popover.tip)
-          return;
-      this.popover.hide();
-    });
-  }
-  init_popover() {
-    this.popover = new bootstrap.Popover(this.element, {
-      container: "body",
-      content: this.route_type(),
-      html: !0,
-      customClass: "dark-editable",
-      title: this.title
-    }), this.element.addEventListener("show.bs.popover", () => {
-      this._element.element.value = this.value, this.hideError(), this.element.dispatchEvent(new CustomEvent("show"));
-    }), this.element.addEventListener("shown.bs.popover", () => {
-      this.element.dispatchEvent(new CustomEvent("shown"));
-    }), this.element.addEventListener("hide.bs.popover", () => {
-      this.element.dispatchEvent(new CustomEvent("hide"));
-    }), this.element.addEventListener("hidden.bs.popover", () => {
-      this.element.dispatchEvent(new CustomEvent("hidden"));
-    });
   }
   /* INIT METHODS END */
   route_type() {
@@ -131,7 +204,7 @@ class d {
   /* TYPES END */
   /* ADD FOCUS */
   add_focus(e) {
-    this.element.addEventListener("shown.bs.popover", function() {
+    this.element.addEventListener("shown", function() {
       e.focus();
     });
   }
@@ -150,7 +223,7 @@ class d {
     this._element.error.style.display = "block";
   }
   hideError() {
-    this._element.error.style.display = "none";
+    this._element.error && (this._element.error.style.display = "none");
   }
   /* DIV ERROR END */
   /* CONTAINER DIV */
@@ -166,19 +239,19 @@ class d {
     const t = document.createElement("form");
     return t.classList.add("d-flex", "align-items-start"), t.style.gap = "20px", t.addEventListener("submit", async (s) => {
       s.preventDefault();
-      const i = e.value;
-      if (this.send && this.pk && this.url && this.value != i) {
+      const n = e.value;
+      if (this.send && this.pk && this.url && this.value != n) {
         this.load(!0);
         let a;
         try {
-          const r = await this.ajax(i);
-          r.ok ? a = await this.success(r, i) : a = await this.error(r, i) || `${r.status} ${r.statusText}`;
+          const r = await this.ajax(n);
+          r.ok ? a = await this.success(r, n) : a = await this.error(r, n) || `${r.status} ${r.statusText}`;
         } catch (r) {
           console.error(r), a = r;
         }
-        a ? (this.setError(a), this.showError()) : (this.setError(null), this.hideError(), this.value = e.value, this.popover.hide(), this.init_text()), this.load(!1);
+        a ? (this.setError(a), this.showError()) : (this.setError(null), this.hideError(), this.value = e.value, this.modeElement.hide(), this.init_text()), this.load(!1);
       } else
-        this.value = e.value, this.popover.hide(), this.init_text();
+        this.value = e.value, this.modeElement.hide(), this.init_text();
       this.element.dispatchEvent(new CustomEvent("save"));
     }), t;
   }
@@ -203,7 +276,7 @@ class d {
     e.classList.add("btn-danger");
     const t = document.createElement("div");
     return t.innerHTML = "✖", e.append(t), e.addEventListener("click", () => {
-      this.popover.hide();
+      this.modeElement.hide();
     }), e;
   }
   /* BUTTONS END */
@@ -212,8 +285,8 @@ class d {
     let t = this.url;
     const s = new FormData();
     s.append("pk", this.pk), s.append("name", this.name), s.append("value", e);
-    const i = {};
-    return i.method = this.ajaxOptions.method, i.method == "POST" ? i.body = s : t += "?" + new URLSearchParams(s).toString(), fetch(t, i);
+    const n = {};
+    return n.method = this.ajaxOptions.method, n.method == "POST" ? n.body = s : t += "?" + new URLSearchParams(s).toString(), fetch(t, n);
   }
   async success(e, t) {
   }
@@ -222,10 +295,10 @@ class d {
   /* AJAX END */
   /* METHODS */
   enable() {
-    this.disabled = !1, this.element.classList.remove("dark-editable-element-disabled"), this.popover.enable();
+    this.disabled = !1, this.element.classList.remove("dark-editable-element-disabled"), this.modeElement.enable();
   }
   disable() {
-    this.disabled = !0, this.element.classList.add("dark-editable-element-disabled"), this.popover.disable();
+    this.disabled = !0, this.element.classList.add("dark-editable-element-disabled"), this.modeElement.enable();
   }
   setValue(e) {
     this.value = e, this.init_text();
@@ -236,5 +309,5 @@ class d {
   /* METHODS END */
 }
 export {
-  d as default
+  E as default
 };
